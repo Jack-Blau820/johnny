78433b758718e8e81c6564c1bb830e5402432840
Jack <sapphiredrago@outlook.com>
2023-11-28 17:54:17 -0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx', // Specify the entry point here
+    },
+  },
   // base:'/',
   // root: './src'
   
